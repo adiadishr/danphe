443e92ba7917c465c920bf913d1a873db71b7a33
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-04-25 14:54:06 +0545

    hero fix link tala ko

--- src/components/Launch/Hero.jsx
+++ src/components/Launch/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     <div className="text28 text-neutral-100">
                         We are committed to designing and manufacturing innovative rocket engines that propel us towards the stars
                     </div>
-                    <div className='textLink text-white border-white   w-80 border-b-2 pb-4 justify-between flex'>
+                    <div className='textLink text-white border-white  w-[75%] md:w-80 border-b-2 pb-4 justify-between flex'>
                         <div className="flex justify-between w-full hover:translate-y-[-5px] transition-all duration-300 cursor-pointer group">
                             Read more about us
                             <div className='flex items-center justify-center font-semibold group-hover:rotate-[-45deg] transition-all duration-300 ease-in-out'><FaArrowRight /></div>
